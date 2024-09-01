af2d3c5f71ceb09a877edaece4d3fa50ed5979d7
unknown <fred@main.LCCA.NET>
2024-09-01 08:39:27 -0400

    Updates

---
+++ assets/GoBackButtonWithArrow-4TIJ-XEZ.js
@@ -0,0 +1,1 @@
+import{u as o,j as t}from"./index-D6gqMyFK.js";import{ad as s}from"./index-CmSZZEqd.js";function c(){const e=o(),a=()=>{e(-1)};return t.jsxs("button",{onClick:a,className:"flex items-center mb-4 text-blue-500 hover:text-blue-700",children:[t.jsx(s,{className:"mr-2"}),"Go Back"]})}export{c as G};
--- assets/GoBackButtonWithArrow-CE4Qy_4D.js
+++
@@ -1,1 +0,0 @@
-import{u as o,j as t}from"./index-ZYBAOPRB.js";import{ad as s}from"./index-DmV7JgmL.js";function c(){const e=o(),a=()=>{e(-1)};return t.jsxs("button",{onClick:a,className:"flex items-center mb-4 text-blue-500 hover:text-blue-700",children:[t.jsx(s,{className:"mr-2"}),"Go Back"]})}export{c as G};
